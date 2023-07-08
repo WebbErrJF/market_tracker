753a3318eb2b7ffbe5a1556d619fd42562486625
Jakub <kuba97fryc@gmail.com>
2023-07-08 13:08:56 +0200

    problem with scheduler

--- market_tracker/api_fetcher/apps.py
+++ market_tracker/api_fetcher/apps.py
@@ -4,7 +4,3 @@ from django.apps import AppConfig
 class ApiFetcherConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'api_fetcher'
-
-    def ready(self):
-        from . import tasks_run
-        tasks_run.start()
--- market_tracker/api_fetcher/celery_app.py
+++ market_tracker/api_fetcher/celery_app.py
@@ -1,5 +1,7 @@
+import django
+django.setup()
 from celery import Celery
-from market_tracker.api_fetcher.tasks import update_stock_db
+from .tasks import update_stock_db
 
 scheduler = {"time_scheduler": {"task": "api_fetcher.celery_app.process_data", "schedule": 30.0}}
 celery_obj = Celery("api_fetcher", broker="redis://redis:6379/0", backend="redis://redis:6379/0", beat_schedule=scheduler)
